86ed87c3715d14ef9e4de354317c377d352f2b3e
wupojung <wupojung@gmail.com>
2023-07-04 03:10:57 +0800

    add led on off

--- RPI-Web/Controllers/HomeController.cs
+++ RPI-Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Device.Gpio;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RPI_Web.Models;
 
@@ -7,10 +8,14 @@ namespace RPI_Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private GpioController _controller;
+    private int pin = 10;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _controller = new GpioController(PinNumberingScheme.Board);
+        _controller.OpenPin(pin, PinMode.Output);
     }
 
     public IActionResult Index()
@@ -20,6 +25,19 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
+        return View();
+    }
+
+
+    public IActionResult On()
+    {
+        _controller.Write(pin, PinValue.High);
+        return View();
+    }
+
+    public IActionResult Off()
+    {
+        _controller.Write(pin, PinValue.Low);
         return View();
     }
 
